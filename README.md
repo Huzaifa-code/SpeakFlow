3f245c1030ca199ca331ae8552ad9e0b55cd06df
Huzaifa-code <huzaifaqureshi037@gmail.com>
2024-10-18 17:33:53 +0530

    SpeakFlow

---
+++ README.md
@@ -0,0 +1,6 @@
+# SpeakFlow
+
+Welcome to SpeakFlow
+Effortlessly convert your speech into text and translate it into any language.
+
+React web based transcription & translation app that uses web workers to run ML models in the browser
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+export default function Header() {
+    return (
+        <header className='flex items-center justify-between gap-4 p-4'>
+            <a href="/"><h1 className='font-medium'>Speak<span className='text-indigo-400 bold'>Flow</span></h1></a>
+            <div className='gap-4 flex items-center '>
+                
+            </div>
+        </header>
+    )
+}
